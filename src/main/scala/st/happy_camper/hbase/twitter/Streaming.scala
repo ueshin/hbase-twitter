8998dd7f6cef5a1d2601838a8ab15a77302721c4
Takuya UESHIN <ueshin@happy-camper.st>
2010-07-02 21:09:29 +0900

    ストリーミングの雛形。
    git-svn-id: https://happy-camper.st/svn/hadoop/trunk/hbase-twitter@76 0d84e6f8-11d7-4a69-a61e-9ccb1fa030a1

---
+++ src/main/scala/st/happy_camper/hbase/twitter/Streaming.scala
@@ -0,0 +1,91 @@
+package st.happy_camper.hbase.twitter
+
+import _root_.st.happy_camper.hbase.twitter.handler.HTableHandler
+
+class Streaming(id: String, pw: String) {
+
+  import _root_.java.io.InputStream
+  import _root_.java.net.{ Authenticator, PasswordAuthentication, URL }
+
+  import _root_.scala.collection.mutable.ListBuffer
+  import _root_.scala.io.Source
+
+  import _root_.scala.actors.{ Actor, TIMEOUT }
+
+  val url = new URL("http://stream.twitter.com/1/statuses/sample.xml?delimited=length")
+
+  Authenticator.setDefault(new Authenticator {
+    override def getPasswordAuthentication = {
+      new PasswordAuthentication(id, pw.toCharArray)
+    }
+  })
+
+  val handlers = ListBuffer[String => Unit]()
+
+  private var in: InputStream = null
+
+  def start {
+    streaming.getState match {
+      case Actor.State.New => {
+        in = url.openStream
+        streaming.start
+      }
+      case Actor.State.Terminated => throw new IllegalStateException("Streaming was closed.")
+      case _ => throw new IllegalStateException("Streaming has already been started.")
+    }
+  }
+
+  def close {
+    streaming.getState match {
+      case Actor.State.New => throw new IllegalStateException("Streaming has not been started yet.")
+      case Actor.State.Terminated => throw new IllegalStateException("Streaming was closed.")
+      case _ => {
+        streaming !? CLOSE
+        if(in != null) {
+          in.close
+        }
+      }
+    }
+  }
+
+  private case class CLOSE()
+
+  private object streaming extends Actor {
+    def act = {
+      val source = Source.fromInputStream(in)
+
+      def readState = {
+        val length = source.getLine(1).replaceAll("\\s", "").toInt
+        source.take(length) mkString
+      }
+
+      loop {
+        receiveWithin(0) {
+          case TIMEOUT => {
+            val state = readState
+            handlers foreach { handler => handler(state) }
+          }
+          case CLOSE => {
+            reply()
+            exit
+          }
+        }
+      }
+    }
+  }
+
+}
+
+object Streaming {
+  def main(args : Array[String]) {
+    val streaming = new Streaming(args(0), args(1))
+    streaming.handlers += new HTableHandler
+    try {
+      streaming.start
+      Stream.continually(System.in.read) takeWhile(_ != -1)
+    }
+    finally {
+      streaming.close
+    }
+  }
+}
---
+++ src/main/scala/st/happy_camper/hbase/twitter/handler/HTableHandler.scala
@@ -0,0 +1,7 @@
+package st.happy_camper.hbase.twitter.handler
+
+class HTableHandler extends (String => Unit) {
+
+  def apply(xml: String) {
+  }
+}
