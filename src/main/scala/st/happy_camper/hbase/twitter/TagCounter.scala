84e13c008bbe154a4a3560eb52d15d8476dbd7b3
Takuya UESHIN <ueshin@happy-camper.st>
2010-08-09 20:41:35 +0900

    定数名を変更。
    git-svn-id: https://happy-camper.st/svn/hadoop/trunk/hbase-twitter@95 0d84e6f8-11d7-4a69-a61e-9ccb1fa030a1

--- src/main/scala/st/happy_camper/hbase/twitter/TagCounter.scala
+++ src/main/scala/st/happy_camper/hbase/twitter/TagCounter.scala
@@ -25,13 +25,13 @@ object TagCounter {
 
     type Context = Mapper[ImmutableBytesWritable, Result, Text, LongWritable]#Context
 
-    private val hashTagRegexp = """#[0-9a-zA-Z_]+""".r
+    private val HashTagRegexp = """#[0-9a-zA-Z_]+""".r
 
     private val one = new LongWritable(1L)
 
     override def map(key: ImmutableBytesWritable, value: Result, context: Context) {
       for((statusKey, StatusWritable(status)) <- value.getFamilyMap("status") if status.user.lang == "ja") {
-        hashTagRegexp findAllIn(status.text) foreach {
+        HashTagRegexp findAllIn(status.text) foreach {
           tag => context.write(new Text(tag.toLowerCase), one)
         }
       }
