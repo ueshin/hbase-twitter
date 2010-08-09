f2e5850ac0697171c533e41aaec5372b179add7d
Takuya UESHIN <ueshin@happy-camper.st>
2010-08-09 22:21:08 +0900

    タグを抽出。
    git-svn-id: https://happy-camper.st/svn/hadoop/trunk/hbase-twitter@96 0d84e6f8-11d7-4a69-a61e-9ccb1fa030a1

--- src/main/resources/create.rb
+++ src/main/resources/create.rb
@@ -1,1 +1,2 @@
 create 'twitter', { NAME => 'user' }, { NAME => 'status' }
+create 'tagtrend', { NAME => 'timeline', VERSIONS => java.lang.Integer::MAX_VALUE }
---
+++ src/main/scala/st/happy_camper/hbase/twitter/TagTransposer.scala
@@ -0,0 +1,59 @@
+package st.happy_camper.hbase.twitter
+
+import _root_.st.happy_camper.hbase.twitter.io.StatusWritable
+
+import _root_.scala.collection.JavaConversions._
+
+import _root_.org.apache.hadoop.hbase.HBaseConfiguration
+import _root_.org.apache.hadoop.hbase.client.{ Scan, Result, Put }
+import _root_.org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import _root_.org.apache.hadoop.hbase.mapreduce.{ TableMapper, IdentityTableReducer, TableMapReduceUtil }
+import _root_.org.apache.hadoop.hbase.util.Bytes
+
+import _root_.org.apache.hadoop.fs.Path
+import _root_.org.apache.hadoop.mapreduce.{ Job, Mapper }
+import _root_.org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+import _root_.org.apache.hadoop.util.GenericOptionsParser
+
+object TagTransposer {
+
+  private implicit def stringToBytes(s: String) = Bytes.toBytes(s)
+
+  class TagTransposeMapper extends TableMapper[ImmutableBytesWritable, Put] {
+
+    type Context = Mapper[ImmutableBytesWritable, Result, ImmutableBytesWritable, Put]#Context
+
+    private val HashTagRegexp = """#[0-9a-zA-Z_]+""".r
+
+    override def map(key: ImmutableBytesWritable, value: Result, context: Context) {
+      val userKey = Bytes.toString(key.get)
+      value.raw foreach {
+        keyvalue => {
+          val statusKey = Bytes.toString(keyvalue.getQualifier)
+          val timestamp = keyvalue.getTimestamp
+          val StatusWritable(status) = keyvalue.getValue
+          HashTagRegexp findAllIn(status.text) foreach {
+            tag => {
+              context.write(key, new Put(tag.toLowerCase).add("timeline", userKey, timestamp, statusKey))
+            }
+          }
+        }
+      }
+    }
+  }
+
+  def main(args: Array[String]) {
+    val conf = new HBaseConfiguration
+    val otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs
+
+    val job = new Job(conf, "Tag Transposer")
+    job.setJarByClass(getClass)
+
+    val offset = (System.currentTimeMillis / (60*60*1000L))
+    val scan = new Scan().addFamily("status").setTimeRange((offset - 1) * (60*60*1000L), offset * (60*60*1000L))
+    TableMapReduceUtil.initTableMapperJob("twitter", scan, classOf[TagTransposeMapper], classOf[ImmutableBytesWritable], classOf[Put], job)
+    TableMapReduceUtil.initTableReducerJob("tagtrend", classOf[IdentityTableReducer], job)
+
+    System.exit(if(job.waitForCompletion(true)) 0 else 1)
+  }
+}
