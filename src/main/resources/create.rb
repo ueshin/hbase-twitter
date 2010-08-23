0eb2f6375ad899ad2d0ecb5b3fed75c62e75b320
Takuya UESHIN <ueshin@happy-camper.st>
2010-08-24 05:45:14 +0900

    TagTransposerの仕様変更に紐づくテーブル変更。
    git-svn-id: https://happy-camper.st/svn/hadoop/trunk/hbase-twitter@107 0d84e6f8-11d7-4a69-a61e-9ccb1fa030a1

--- src/main/resources/create.rb
+++ src/main/resources/create.rb
@@ -1,3 +1,3 @@
 create 'twitter', { NAME => 'user' }, { NAME => 'status' }
 create 'configuration', { NAME => 'property', VERSIONS => java.lang.Integer::MAX_VALUE }
-create 'tagtrend', { NAME => 'timeline', VERSIONS => java.lang.Integer::MAX_VALUE }, { NAME => 'hourly' }
+create 'tagtrend', { NAME => 'timeline' }, { NAME => 'hourly' }
