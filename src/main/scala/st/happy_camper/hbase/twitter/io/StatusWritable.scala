ff877bb26895207e57bf3f6a571bd4f5a07e6da3
Takuya UESHIN <ueshin@happy-camper.st>
2010-11-07 05:11:51 +0900

    MessagePackを使ってみる。
    git-svn-id: https://happy-camper.st/svn/hadoop/trunk/hbase-twitter@124 0d84e6f8-11d7-4a69-a61e-9ccb1fa030a1

--- src/main/scala/st/happy_camper/hbase/twitter/io/StatusWritable.scala
+++ src/main/scala/st/happy_camper/hbase/twitter/io/StatusWritable.scala
@@ -2,6 +2,7 @@ package st.happy_camper.hbase.twitter
 package io
 
 import entity.Status
+import template.StatusTemplate
 
 import _root_.java.io.{ DataInput, DataOutput }
 import _root_.java.util.Date
@@ -9,6 +10,8 @@ import _root_.java.util.Date
 import _root_.org.apache.hadoop.hbase.util.Writables
 
 import _root_.org.apache.hadoop.io.{ Text, Writable, WritableUtils }
+
+import _root_.org.msgpack.MessagePack
 
 private class StatusWritable(var status: Status = null) extends Writable {
 
@@ -39,30 +42,15 @@ object StatusWritable {
   }
 
   def write(out: DataOutput, status: Status) {
-    WritableUtils.writeVLong(out, status.createdAt.getTime)
-    WritableUtils.writeVLong(out, status.id)
-    Text.writeString(out, status.text)
-    Text.writeString(out, status.source)
-    out.writeBoolean(status.truncated)
-    WritableUtils.writeVLong(out, status.inReplyToStatusId)
-    WritableUtils.writeVLong(out, status.inReplyToUserId)
-    out.writeBoolean(status.favorited)
-    Text.writeString(out, status.inReplyToScreenName)
-    UserWritable.write(out, status.user)
+    val bytes = MessagePack.pack(status, StatusTemplate)
+    WritableUtils.writeVInt(out, bytes.length)
+    out.write(bytes)
   }
 
   def read(in: DataInput) = {
-    new Status(
-      new Date(WritableUtils.readVLong(in)),
-      WritableUtils.readVLong(in),
-      Text.readString(in),
-      Text.readString(in),
-      in.readBoolean(),
-      WritableUtils.readVLong(in),
-      WritableUtils.readVLong(in),
-      in.readBoolean(),
-      Text.readString(in),
-      UserWritable.read(in)
-    )
+    val length = WritableUtils.readVInt(in)
+    val bytes = new Array[Byte](length)
+    in.readFully(bytes)
+    MessagePack.unpack(bytes, StatusTemplate).asInstanceOf[Status]
   }
 }
