c51682430142645dccc2a393dceeafc4d8815ada
Takuya UESHIN <ueshin@happy-camper.st>
2011-02-19 17:52:33 +0900

    初期インポート。
    git-svn-id: https://happy-camper.st/svn/hadoop/trunk/hbase-twitter@154 0d84e6f8-11d7-4a69-a61e-9ccb1fa030a1

--- src/main/scala/st/happy_camper/hbase/twitter/mapreduce/Mappers.scala
+++
@@ -1,9 +0,0 @@
-package st.happy_camper.hbase.twitter
-package mapreduce
-
-import _root_.org.apache.hadoop.mapreduce.Mapper
-
-trait Mappers[KEYIN, VALUEIN, KEYOUT, VALUEOUT] extends Mapper[KEYIN, VALUEIN, KEYOUT, VALUEOUT] {
-
-  type Context = Mapper[KEYIN, VALUEIN, KEYOUT, VALUEOUT]#Context
-}
